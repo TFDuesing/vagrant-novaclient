f55b9504ed2a94ae2f44b35d9794e3318d5da27f
Thomas Duesing <TFDuesing@gmail.com>
2013-10-17 13:04:36 -0500

    clean up boostrap script
    As of Vagrant 1.3.0, `vagrant up` will only run provisioning by default
    the first time it is run, which allowed for some cleanup.  Also, pip’s
    VCS support is like magic.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,6 @@
 
 user="$1"
 user_dir="/home/$user"
-source_dir="$user_dir/code"
 synced_dir="$2"
 
 
@@ -12,15 +11,15 @@ echo "  Updating and Installing Prerequisite Packages"
 echo "*************************************************************************"
 echo ""
 
-/usr/bin/yum --assumeyes --quiet update yum
-/usr/bin/yum --assumeyes --quiet --exclude kernel* update
+/usr/bin/yum --assumeyes update yum
+/usr/bin/yum --assumeyes --exclude=kernel* update
 
 # python-devel is required to build the (optional) C extension for simplejson
-/usr/bin/yum --assumeyes --quiet install vim git python-pip python-devel
+/usr/bin/yum --assumeyes install git python-devel python-pip
 
 # pycrypto is required for CryptedFileKeyring support,
 # which is necessary for supernova-keyring to encrypt passwords
-/usr/bin/pip install --upgrade --quiet pycrypto
+/usr/bin/pip install --quiet pycrypto
 
 
 echo ""
@@ -30,59 +29,20 @@ echo "*************************************************************************"
 echo ""
 
 # install rackspace-novaclient
-/usr/bin/pip install --upgrade --quiet rackspace-novaclient
-
-/usr/bin/mkdir --parents $source_dir
+/usr/bin/pip install rackspace-novaclient
 
 # install python-novaclient
-if [ -d "$source_dir/python-novaclient" ]; then
-    cd $source_dir/python-novaclient
-    /usr/bin/git pull --quiet
-    /usr/bin/pip install --quiet \
-        --requirement $source_dir/python-novaclient/requirements.txt
-    /usr/bin/python $source_dir/python-novaclient/setup.py --quiet install
-    cd
-else
-    /usr/bin/git clone --quiet \
-        https://github.com/openstack/python-novaclient.git \
-        $source_dir/python-novaclient
-    cd $source_dir/python-novaclient
-    /usr/bin/pip install --quiet \
-        --requirement $source_dir/python-novaclient/requirements.txt
-    /usr/bin/python $source_dir/python-novaclient/setup.py --quiet install
-    cd
-fi
+/usr/bin/pip install --upgrade git+https://git.openstack.org/openstack/python-novaclient.git@master#egg=python-novaclient
 
 # install supernova
+/usr/bin/pip install git+https://github.com/major/supernova.git@master#egg=supernova
 
-if [ -d "$source_dir/supernova" ]; then
-    cd $source_dir/supernova
-    /usr/bin/git pull --quiet
-    /usr/bin/pip install --quiet \
-        --requirement $source_dir/supernova/requirements.txt
-    /usr/bin/python $source_dir/supernova/setup.py --quiet install
-    cd
-else
-    /usr/bin/git clone --quiet https://github.com/major/supernova.git \
-        $source_dir/supernova
-    cd $source_dir/supernova
-    /usr/bin/pip install --quiet \
-        --requirement $source_dir/supernova/requirements.txt
-    /usr/bin/python $source_dir/supernova/setup.py --quiet install
-    cd
-fi
-
-/usr/bin/chown --recursive $user:$user $source_dir
-
-# add symlink to .supernova file
+# add symlink from supernova config file (with or withour the dot)
+# to ~/.supernova file
 if [ -f "$synced_dir/supernova" ]; then
-    if [ ! -h "$user_dir/.supernova" ]; then
-        /usr/bin/ln --symbolic $synced_dir/supernova $user_dir/.supernova
-    fi
+  /usr/bin/ln --symbolic $synced_dir/supernova $user_dir/.supernova
 elif [ -f "$synced_dir/.supernova" ]; then
-    if [ ! -h "$user_dir/.supernova" ]; then
-        /usr/bin/ln --symbolic $synced_dir/.supernova $user_dir/.supernova
-    fi
+  /usr/bin/ln --symbolic $synced_dir/.supernova $user_dir/.supernova
 else
     echo "[Whoops!] No supernova file found."
 fi
